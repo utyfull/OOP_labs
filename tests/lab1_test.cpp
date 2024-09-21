06594a821e66f9efd00cb51ed27d03eb4e1fa284
utyfull <serg_misha1@mail.ru>
2024-09-21 12:41:47 +0000

    update test to AAA

--- tests/lab1_test.cpp
+++ tests/lab1_test.cpp
@@ -3,42 +3,51 @@
 
 TEST(GcdTest, PositiveNumbers)
 {
-    EXPECT_EQ(CalculateGcd(48, 18), 6);
-    EXPECT_EQ(CalculateGcd(101, 10), 1);
-    EXPECT_EQ(CalculateGcd(56, 98), 14);
-    EXPECT_EQ(CalculateGcd(123, 456), 3);
+    int n{48};
+    int m{18};
+
+    int result = CalculateGcd(n, m);
+
+    ASSERT_TRUE(result == 9);
+    
 }
 
 TEST(GcdTest, SameNumbers)
 {
-    EXPECT_EQ(CalculateGcd(25, 25), 25);
-    EXPECT_EQ(CalculateGcd(1000, 1000), 1000);
+    int n{25};
+    int m{25};
+
+    int result = CalculateGcd(n, m);
+
+    ASSERT_TRUE(result == 25);
 }
 
 TEST(GcdTest, ZeroInput)
 {
-    EXPECT_EQ(CalculateGcd(0, 5), 5);
-    EXPECT_EQ(CalculateGcd(5, 0), 5);
-    EXPECT_EQ(CalculateGcd(0, 0), 0);
+    int n{0};
+    int m{0};
+
+    int result = CalculateGcd(n, m);
+
+    ASSERT_TRUE(result == 0);
 }
 
 TEST(GcdTest, OneInput)
 {
-    EXPECT_EQ(CalculateGcd(1, 5), 1);
-    EXPECT_EQ(CalculateGcd(5, 1), 1);
-    EXPECT_EQ(CalculateGcd(1, 1), 1);
+    int n{1};
+    int m{5};
+
+    int result = CalculateGcd(n, m);
+
+    ASSERT_TRUE(result == 1);
 }
 
 TEST(GcdTest, LargeNumbers)
 {
-    EXPECT_EQ(CalculateGcd(123456789, 987654321), 9);
-    EXPECT_EQ(CalculateGcd(1000000000, 500000000), 500000000);
-}
+    int n{123456789};
+    int m{987654321};
 
-TEST(GcdTest, OneNumberGreater)
-{
-    EXPECT_EQ(CalculateGcd(7, 14), 7);
-    EXPECT_EQ(CalculateGcd(100, 25), 25);
-    EXPECT_EQ(CalculateGcd(500, 1000), 500);
-    EXPECT_EQ(CalculateGcd(2000, 500), 500);
+    int result = CalculateGcd(n, m);
+
+    ASSERT_TRUE(result == 9);
 }
