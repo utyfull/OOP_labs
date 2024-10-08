285a0721738908d23d732c2d3d36f30d221c7f5b
Utyfull <serg_misha1@mail.ru>
2024-10-08 14:30:09 +0000

    create lab3 without testing

--- lab3/include/lab3.h
+++ lab3/include/lab3.h
@@ -2,30 +2,34 @@
 #define LAB3_H
 
 #include <iostream>
+#include <stdexcept>
+#include <vector>
+#include <memory>
 
 class Figure {
 protected:
-    double _centerX;
-    double _centerY;
+    mutable double _area;
+    mutable bool _areaCalculated;
+    mutable double _centerX, _centerY;
+    mutable bool _centerCalculated;
+
 public:
-    Figure(): _centerX(0.0), _centerY(0.0) {}
+    Figure() : _area(0), _areaCalculated(false), _centerX(0), _centerY(0), _centerCalculated(false) {}
+    double getArea() const;
+    double getFigureCenter() const;
 
-    virtual void calculateFigureCenter() const {}
-
-    friend std::ostream&operator<<(std::ostream& os, const Figure& figure) {}
-    friend std::istream&operator>>(std::istream is, Figure& figure) {}
-    virtual operator double() const {}
+    virtual void calculateFigureCenter() const = 0;
+    virtual operator double() const = 0;
 
     virtual ~Figure() {}
 };
 
 
-
 class Array {
 protected:
-    Figure* _elements;
-    size_t _capacity;
-    size_t _size;
+    Figure** _elements;  
+    size_t _capacity;   
+    size_t _size;        
 
 public:
     Array(); 
@@ -38,9 +42,61 @@ public:
     Array& operator=(Array&& other) noexcept;
 
     size_t size() const;
+
+    void addFigure(Figure* figure);
+    void removeFigure(size_t index);
+    void printFigures() const;
+    double totalArea() const;
 };
 
-class
+class Trapezoid: public Figure {
+private:
+    double __x1, __x2, __x3, __x4, __y1, __y2, __y3, __y4;
+public:
+    Trapezoid(): __x1(0), __x2(0), __x3(0), __x4(0), __y1(0), __y2(0), __y3(0), __y4(0) {}
+
+    void calculateFigureCenter() const override;
+    operator double() const override;
+
+    Trapezoid& operator=(const Trapezoid& other);
+    Trapezoid& operator=(Trapezoid&& other) noexcept;
+    bool operator==(const Trapezoid& other) const;
+    friend std::ostream& operator<<(std::ostream& os, const Trapezoid& trapezoid);
+    friend std::istream& operator>>(std::istream& is, Trapezoid& trapezoid);
+};
+
+class Rhomb: public Figure {
+private:
+    double __x1, __x2, __x3, __x4, __y1, __y2, __y3, __y4;
+public:
+    Rhomb(): __x1(0), __x2(0), __x3(0), __x4(0), __y1(0), __y2(0), __y3(0), __y4(0) {}
+
+    void calculateFigureCenter() const override;
+    operator double() const override;
+
+    Rhomb& operator=(const Rhomb& other);
+    Rhomb& operator=(Rhomb&& other) noexcept;
+    bool operator==(const Rhomb& other) const;
+    friend std::ostream& operator<<(std::ostream& os, const Rhomb& rhomb);
+    friend std::istream& operator>>(std::istream& is, Rhomb& rhomb);
+};
+
+class Pentagon: public Figure {
+private:
+    double __x1, __x2, __x3, __x4, __x5, __y1, __y2, __y3, __y4, __y5;
+public:
+    Pentagon(): __x1(0), __x2(0), __x3(0), __x4(0), __x5(0), __y1(0), __y2(0), __y3(0), __y4(0), __y5(0) {}
+
+    void calculateFigureCenter() const override;
+    operator double() const override;
+
+    Pentagon& operator=(const Pentagon& other);
+    Pentagon& operator=(Pentagon&& other) noexcept;
+    bool operator==(const Pentagon& other) const;
+    friend std::ostream& operator<<(std::ostream& os, const Pentagon& pentagon);
+    friend std::istream& operator>>(std::istream& is, Pentagon& pentagon);
+};
+
 
 
 #endif
--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,1 +1,43 @@
 #include "lab3.h"
+
+int main() {
+    Array array(10);  // Создаем массив с максимальной вместимостью 10 фигур
+
+    // Создаем фигуры
+    Trapezoid* trapezoid = new Trapezoid();
+    Rhomb* rhomb = new Rhomb();
+    Pentagon* pentagon = new Pentagon();
+
+    // Инициализируем фигуры (например, с помощью оператора >>)
+    std::cout << "Введите координаты для трапеции (x1 y1 x2 y2 x3 y3 x4 y4): ";
+    std::cin >> *trapezoid; // предполагаем, что оператор >> перегружен
+
+    std::cout << "Введите координаты для ромба (x1 y1 x2 y2 x3 y3 x4 y4): ";
+    std::cin >> *rhomb;
+
+    std::cout << "Введите координаты для пятиугольника (x1 y1 x2 y2 x3 y3 x4 y4 x5 y5): ";
+    std::cin >> *pentagon;
+
+    // Добавляем фигуры в массив
+    array.addFigure(trapezoid);
+    array.addFigure(rhomb);
+    array.addFigure(pentagon);
+
+    // Печатаем геометрические центры и площади
+    array.printFigures();
+
+    // Печатаем общую площадь
+    std::cout << "Общая площадь фигур в массиве: " << array.totalArea() << std::endl;
+
+    // Удаляем фигуру по индексу (например, удаляем фигуру с индексом 1)
+    array.removeFigure(1);
+
+    // Печатаем фигуры после удаления
+    std::cout << "После удаления фигуры с индексом 1:" << std::endl;
+    array.printFigures();
+
+    // Очистка памяти (необходимо, если используется raw указатели)
+    // Поскольку удаление происходит в деструкторе массива, мы можем просто выйти из программы
+
+    return 0;
+}
