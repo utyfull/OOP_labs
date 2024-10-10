7132bc2ddb1ba28c7669c50617e84551a09ea87e
utyfull <serg_misha1@mail.ru>
2024-10-10 14:15:14 +0000

    add totalAreatests

--- tests/lab3_test.cpp
+++ tests/lab3_test.cpp
@@ -1,6 +1,49 @@
 #include "lab3.h"
 #include <gtest/gtest.h>
 
+TEST(FigureTest, ArrayTotalArea) {
+    Array array(10);  
+    Trapezoid* trapezoid = new Trapezoid();
+    Rhomb* rhomb = new Rhomb();
+    Pentagon* pentagon = new Pentagon();
+    std::istringstream is1("1 2 3 4 5 6 7 8");
+    std::istringstream is2("1 2 3 4 5 6 7 8");
+    std::istringstream is3("1 2 3 4 5 6 7 8 9 10");
+    is1 >> *rhomb;
+    is2 >> *trapezoid;
+    is3 >> *pentagon;
+    array.addFigure(trapezoid);
+    array.addFigure(rhomb);
+    array.addFigure(pentagon);
+
+    double totalArea = array.totalArea();
+
+    ASSERT_NEAR(totalArea, 25, 0.5);
+}
+
+
+TEST(FigureTest, ArrayTotalAreaWithDeleteElement) {
+    Array array(10);  
+    Trapezoid* trapezoid = new Trapezoid();
+    Rhomb* rhomb = new Rhomb();
+    Pentagon* pentagon = new Pentagon();
+    std::istringstream is1("1 2 3 4 5 6 7 8");
+    std::istringstream is2("1 2 3 4 5 6 7 8");
+    std::istringstream is3("1 2 3 4 5 6 7 8 9 10");
+    is1 >> *rhomb;
+    is2 >> *trapezoid;
+    is3 >> *pentagon;
+    array.addFigure(trapezoid);
+    array.addFigure(rhomb);
+    array.addFigure(pentagon);
+    array.removeFigure(1);
+    
+    double totalArea = array.totalArea();
+
+    ASSERT_NEAR(totalArea, 9, 0.5);
+}
+
+
 TEST(FigureTest, PentagonAssignment) {
     Pentagon* pentagon1 = new Pentagon();
     Pentagon* pentagon2 = new Pentagon();
